ec78aaf012d55d2e1ffa5c86390de597af5d6e88
slokostic3212 <dsk3212@yahoo.com>
2020-10-24 17:12:09 -0400

    update pages

---
+++ src/test/java/com/weborders/step_definitions/LoginStepDefinitions.java
@@ -0,0 +1,29 @@
+package com.weborders.step_definitions;
+
+import com.weborders.pages.LoginPage;
+import com.weborders.utils.ConfigurationReader;
+import com.weborders.utils.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinitions {
+    LoginPage loginPage = new LoginPage();
+//    instance = object
+
+    @Given("user is on the landing page")
+    public void user_is_on_the_landing_page() {
+        String URL = ConfigurationReader.getProperty("url");
+        Driver.getDriver().get(URL);
+    }
+
+    @When("user logs in")
+    public void user_logs_in() {
+        loginPage.login();
+    }
+
+    @Then("user should see {string} page title")
+    public void user_should_see_page_title(String string) {
+
+    }
+}
